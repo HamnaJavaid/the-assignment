66e18a3a4c0d9731d0e0571c92c5d0101c33191d
HamnaJavaid <106843410+HamnaJavaid@users.noreply.github.com>
2022-06-15 17:09:09 +0500

    Add files via push
    The final assignment

---
+++ AssignmentSE/src/Assign.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class Assign {
+	@Test
+	public static void main(String [] arg) {
+		System.out.println("This method is ready");
+		System.out.println("It has been Compiled and Tested");
+		System.out.println("And I am Hamna Javaid roll no. 31");
+	}
+}
